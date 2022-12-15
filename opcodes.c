c87c2e2e455774b05543b3319ab5a42d59b7899c
overlord-destro <edemcoffie41@gmail.com>
2022-12-15 14:42:36 +0000

    Added pint functionality

--- opcodes.c
+++ opcodes.c
@@ -12,6 +12,7 @@ void (*get_opcodes(char *opc))(stack_t **stack, unsigned int line_number)
 	instruction_t instruct[] = {
 		{"push", _push},
 		{"pall", _pall},
+		{"pint", _pint},
 		{NULL, NULL}
 	};
 	int i;
---
+++ opcodes2.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+
+/**
+ * _pint - prints the value at top of  stack
+ *
+ * @doubly: head of the linked list
+ * @cline: line number
+ * Return: no return
+ */
+void _pint(stack_t **doubly, unsigned int __attribute__ ((unused)) cline)
+{
+	if (*doubly == NULL)
+	{
+		dprintf(2, "L%u: ", cline);
+		dprintf(2, "can't pint, stack empty\n");
+		free_vglo();
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%d\n", (*doubly)->n);
+}
