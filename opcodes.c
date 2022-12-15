02bbb4301449fc68127ccbfb36a4f4e1e905fc1e
overlord-destro <edemcoffie41@gmail.com>
2022-12-15 14:52:21 +0000

    Added swap

--- opcodes.c
+++ opcodes.c
@@ -13,6 +13,8 @@ void (*get_opcodes(char *opc))(stack_t **stack, unsigned int line_number)
 		{"push", _push},
 		{"pall", _pall},
 		{"pint", _pint},
+		{"pop", _pop},
+		{"swap", _swap},
 		{NULL, NULL}
 	};
 	int i;
